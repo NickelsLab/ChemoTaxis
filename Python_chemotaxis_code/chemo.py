1f5ba164d0302fd531bc07f4d241906ccc3da2c4
Kevin Nickels <knickels@trinity.edu>
2015-05-01 16:18:35 -0500

    Starting scaling up (1micro in MatLab == 1cm in stage)

--- Python_chemotaxis_code/chemo.py
+++ Python_chemotaxis_code/chemo.py
@@ -2,20 +2,17 @@
 
 # chemo.py
 
-
 # Simple controller that uses principles of chemotaxis
 # Get concentration from Dr. Nguyen's function
 # Compare methylation state from two points, run or tumble
 # Tim Davison Feb 2015
 
-# to run player server - "player tim_bb.cfg" in one window
+# to run player server - "player PetriDish.cfg" in one window
 # to run controller - "python chemo.py" in another window
 
 import math
 import random
 import time
-#def dtor (deg):
- #   return deg*math.pi/180.0;
 
 import sys, os
 # this should be whereever "playercpp.py" is.  
@@ -24,95 +21,61 @@ sys.path.append('/usr/local/lib/python2.7/site-packages/')
 sys.path.append('/usr/local/lib64/python2.7/site-packages/')
 from playercpp import *
 
-
-#gradient function for inputs center, diameter, position, intensity
-def gradientberg ( Max, Xc, Yc, D, Xpos, Ypos, t):
-        rc_cm = 0.01 #cm
-        rc= rc_cm*1e4 #um
-        r0 = 0.0
-        r = ((Xc-Xpos)**2 + (Yc-Ypos)**2)**0.5
-        r1 = r-r0
-        t1_num = Max*rc*rc #um*um
-        t1_den = 2*r1*(math.pi*D*t)**0.5 #um*um
-        t_exp = math.exp(-(r1*r1)/(4*D*t))
-        den = 1 + (3*rc*r1/(4*D*t))
-        asp = (t1_num/t1_den)*(t_exp/den)
-        return (asp)
-
 #note D is diffusion rate
-def gradient( Max, Xs, Ys, D, Xc, Yc, t ):
+# Xs,Ys = center of chemical plume
+# Xc,Yc = location of sample
+def green_gradient( Max, Xs, Ys, D, Xc, Yc, t ):
 	r2 = (Xc - Xs)**2 + (Yc - Ys)**2
 	G = Max*math.exp(-r2/(4*D*t))/(4*math.pi*D*t)
 	return G
 
-Max = 1.5
+Max = 4 # milli-moles (?)
 Xc = 0.0
 Yc = 0.0
-D = 0.16 
-t = 10.0 #the higher this is, the higher concentration intensity
+# D = 0.16 # moles/micro-meter (??)
+D = 1600 # moles/cm (?)
+t = 2000 # fixed_time
+
 # Make proxies for Client, Sonar, Position2d
 robot = PlayerClient("localhost");
-rp = RangerProxy(robot,0);
+#rp = RangerProxy(robot,0);
 pp = Position2dProxy(robot,0);
 sp = SimulationProxy(robot,0);
 
-#robot.RequestGeometry();
+#rp.RequestConfigure(); # fills up angle structures
 
-rp.RequestConfigure(); # fills up angle structures
+def run(speed, dt):
+	print '-->running at ',speed,' cm/s for ',1000*dt,' ms'
+	pp.SetSpeed(speed, 0)
+	robot.Read()
+	#time.sleep(dt)
+	pp.SetSpeed(0,0)
 
-def run(speed, tm):
-        print 'running'
-        print "\n"
-        pp.SetSpeed(speed, 0)
-        time.sleep(tm)
-
-def tumble(tmax, turnrate):
-        print 'tumbling'
-        print "\n"
-#       pp.SetSpeed(0,turnrate)
-#       time.sleep(random.random()*tmax)
-#       pp.SetSpeed(0,0)
-        t = random.random()-0.5
-        if t >= 0:
-                pp.SetSpeed(0,turnrate)
-                time.sleep(t*tmax)
-                pp.SetSpeed(0,0)
-        else:
-                pp.SetSpeed(0,-1.0*turnrate)
-                time.sleep(-1.0*t*tmax)
-	return abs(t)*tmax
-
+def tumble(turnrate,dt):
+	print '-->tumbling at ', turnrate,' rad/s for ',1000*dt,' ms'
+	pp.SetSpeed(0,turnrate)
+	robot.Read()
+	# time.sleep(dt)
+	pp.SetSpeed(0,0)
 
 def sample(x,y):
 #       print 'sampling'
-        return gradient( Max, Xc, Yc, D, x, y, t)
+        return green_gradient( Max, Xc, Yc, D, x, y, t)
 
-def pos():
-        robot.Read()
-        x = pp.GetXPos()
-        y = pp.GetYPos()
+def nose_pos():
+	nose_dist = 0.08 # dist from centroid to nose
+	robot.Read()
+	x = pp.GetXPos() # position of (centroid of) the robot
+	y = pp.GetYPos()
 	yaw = pp.GetYaw()
-	yaw = pp.GetYaw()
+
 	#this calculates the position of the front of the bot (the nose)
-        newx = x + 0.5*math.cos(yaw)
-        newy = y + 0.5*math.sin(yaw)
-	print 'Xpos:', newx, 'Ypos:', newy
-	return newx, newy
+	nosex = x + nose_dist*math.cos(yaw)
+	nosey = y + nose_dist*math.sin(yaw)
+	print 'Xpos:', nosex, 'Ypos:', nosey
+	return nosex, nosey
 
 
-#def chemo():
-#	x1, y1 = pos()
-#       C1 = sample(x1, y1)
-#       run(0.2,4.0)
-#       x2, y2 = pos()
-#       C2 = sample(x2, y2)
-#       print 'Inital Conc:', C1, 'Current Conc:', C2
-#       if C2 >=  C1:
-#               chemo()
-#       else:
-#               #the product of these two parameters should be 2Pi
-#               tumble(2.0, math.pi)
-#               chemo()
 def eps_val(m):
 	eps = [1.0, 0.5, 0.0, -0.3, -0.6, -0.85, -1.1, -2.0, -3.0]
 	if m <= 0:
@@ -170,29 +133,32 @@ def rapidcell(S, m, dt):
 	return m,mb
 
 def chemo(m,dt):
-	x1, y1 = pos()
+	x1, y1 = nose_pos()
 	C1 = sample(x1, y1)
-	print 'C1', C1
+	print 'C1= ', C1
 	m,mb = rapidcell(C1, m,dt)
-	print mb
+	print 'mb= ', mb
 	if random.random() <  mb:
-		run(0.6,0.3)
-		chemo(mb,0.3)
+		run(0.20,dt) # 20cm/s for dt
 	else:
-		tnew = tumble(4.0,math.pi/2)
-		chemo(mb,tnew)
-		
+		deg_to_tumble = random.randrange(-179,180)
+		rad_to_tumble = deg_to_tumble*math.pi/180.0
+		speed_to_tumble = rad_to_tumble/dt
+		tumble(speed_to_tumble,dt) # random speed for dt
 		
 
 while(1):
-    # read from the proxies
-        robot.Read()
-	x1, y1 = pos()
+	robot.Read()	 # read from the proxies
+# give the m time to hit steady-state
 	m = 5
-        C1 = sample(x1, y1)
-        for x in range (0,40):
-        	time.sleep(0.1)
-                m,mb = rapidcell(C1, m,0.5)
-	print m
-	chemo(m,0.5)
+	x1, y1 = nose_pos()
+	for x in range (0,5):
+		C1 = sample(x1, y1)
+		robot.Read()
+		# time.sleep(0.1)
+		m,mb = rapidcell(C1, m,0.010)
+
+	print 'm=', m
+	for x in range (0,1000):
+		chemo(m,0.010)
 
